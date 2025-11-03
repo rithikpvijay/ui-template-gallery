2202558e2777d725430fd7a96ae0f91794fc0987
Rithik <rithikpvijay2002@gmail.com>
2025-11-03 16:21:15 +0530

    feat(main) : add main content

---
+++ src/components/main/MainHeaderFilter.vue
@@ -0,0 +1,19 @@
+<template>
+  <base-menu>
+    <template #title>
+      <p>ALL</p>
+      <Icon icon="material-symbols:arrow-drop-down" width="16" height="16" />
+    </template>
+
+    <template #menu>
+      <div>All</div>
+      <div>Salaried</div>
+      <div>Commission</div>
+      <div>Terminated</div>
+    </template>
+  </base-menu>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
