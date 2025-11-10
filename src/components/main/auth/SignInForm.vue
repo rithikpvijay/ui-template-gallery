ad3cc1aa09292ff71197b0cc52f07ae9cbd24243
Rithik <rithikpvijay2002@gmail.com>
2025-11-10 21:19:20 +0530

    feat(sign-in): implement sign-in functionality

--- src/components/main/auth/SignInForm.vue
+++ src/components/main/auth/SignInForm.vue
@@ -112,4 +112,12 @@ const handleSignUp = () => {
   margin-top: 20px;
   text-align: center;
 }
+
+.loading {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: var(--color-blue-dark);
+}
 </style>
