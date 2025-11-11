30a4666842bb32316ef4adbc588d7439264db771
Rithik <rithikpvijay2002@gmail.com>
2025-11-11 21:43:56 +0530

    feat(auth): add logout functionality

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -8,18 +8,31 @@
     </div>
 
     <div class="nav-utilities">
-      <div class="search-bar">
+      <div class="search-bar nav-item">
         <base-search-bar placeholder="Search"></base-search-bar>
       </div>
-      <div class="nav-icon">
+      <div class="nav-icon nav-item">
         <Icon icon="material-symbols:dark-mode-outline-rounded" width="20" height="16" />
       </div>
-      <div class="nav-icon">
+      <div class="nav-icon nav-item">
         <Icon icon="mdi:bell-outline" width="20" height="16" />
       </div>
-      <div class="image-container">
-        <img :src="userImage" alt="user-image" class="image" />
-      </div>
+      <base-menu position="bottom-right">
+        <template #title>
+          <div class="image-container nav-item">
+            <img :src="userImage" alt="user-image" class="image" />
+          </div>
+        </template>
+        <template #menu>
+          <div class="log-out" @click="handleLogout">
+            <Icon icon="material-symbols:directions-run-rounded" width="18" height="18" />
+            <p>Logout</p>
+          </div>
+        </template>
+      </base-menu>
+    </div>
+    <div class="overlay" v-if="isLoading">
+      <Icon icon="line-md:loading-loop" width="86" height="86" class="loading" />
     </div>
   </div>
 </template>
@@ -28,6 +41,15 @@
 import { Icon } from '@iconify/vue'
 import userImage from '@/assets/user-image.png'
 import BaseSearchBar from '../UI/BaseSearchBar.vue'
+import { useAuthStore } from '@/store/auth'
+import { storeToRefs } from 'pinia'
+
+const authStore = useAuthStore()
+const { isLoading } = storeToRefs(authStore)
+
+const handleLogout = () => {
+  authStore.logout()
+}
 </script>
 
 <style scoped>
@@ -97,5 +119,31 @@ import BaseSearchBar from '../UI/BaseSearchBar.vue'
 
 .search-bar {
   padding-right: 8px;
+}
+
+.log-out {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  margin-right: 0;
+}
+
+.loading {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  align-items: center;
+  justify-content: center;
+  color: var(--color-blue);
+}
+
+.overlay {
+  position: fixed;
+  height: calc(100vh - 56.5px);
+  width: calc(100vw - 250px);
+  top: 8%;
+  left: 17%;
+  backdrop-filter: blur(2px);
 }
 </style>
