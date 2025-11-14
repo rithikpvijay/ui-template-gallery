168874e025ac88e46e99c3e2dcd304fb785e4cae
Rithik <rithikpvijay2002@gmail.com>
2025-11-14 15:00:42 +0530

    feat(filter-menu): add BaseFilterMenu component and integrate with ContactTable

---
+++ src/components/UI/BaseFilterMenu.vue
@@ -0,0 +1,196 @@
+<template>
+  <div class="container-wrapper" ref="menuRef">
+    <div class="filter-menu-heading">
+      <slot name="title"></slot>
+      <div @click="handleOpen">
+        <slot name="trigger"></slot>
+      </div>
+    </div>
+
+    <Transition name="fade">
+      <div :style="filterMenuStyle" class="base-menu-container" v-if="isOpen">
+        <div class="scroll-area">
+          <div class="header">
+            <input type="checkbox" class="check-box" />
+            <p>Select All</p>
+          </div>
+          <div class="options">
+            <slot name="filter-menu"></slot>
+          </div>
+        </div>
+        <div class="menu-container-buttons">
+          <div @click="handleClose">OK</div>
+          <div @click="handleClose">CANCEL</div>
+        </div>
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
+import type { CSSProperties } from 'vue'
+
+interface Props {
+  header: string
+  position?: 'top-start' | 'top-end' | 'bottom-start' | 'bottom-end'
+  target?: HTMLElement[] | null
+}
+
+const props = defineProps<Props>()
+const menuRef = ref<HTMLElement | null>(null)
+const isOpen = ref(false)
+
+const filterMenuStyle = computed(() => {
+  let targetWidth
+  const baseStyle = ref<CSSProperties>({
+    position: 'absolute',
+    boxShadow: 'var(--shadow-menu-filter)',
+    height: '315px',
+    width: '252px',
+    backgroundColor: 'var(--color-primary)',
+    zIndex: '100',
+    padding: '6px 3px 6px 12px',
+    overflowY: 'hidden',
+    display: 'flex',
+    flexDirection: 'column',
+    color: 'var(--color-text)',
+    fontWeight: '400',
+  })
+
+  if (props.target) {
+    targetWidth = props.target[0]?.offsetWidth
+  }
+
+  if (props.position === 'bottom-end') {
+    return {
+      ...baseStyle.value,
+      top: '100%',
+      left: `calc(${targetWidth}px - ${baseStyle.value.width})`,
+    }
+  }
+
+  if (props.position === 'top-start') {
+    return { ...baseStyle.value, bottom: '100%', left: '0' }
+  }
+
+  if (props.position === 'top-end') {
+    return { ...baseStyle.value, bottom: '100%', right: '0' }
+  }
+
+  return { ...baseStyle.value, top: '100%', left: '0' }
+})
+
+const handleClose = () => {
+  isOpen.value = false
+}
+
+const handleOpen = () => {
+  if (!isOpen.value) {
+    isOpen.value = true
+    return
+  }
+  isOpen.value = false
+  setTimeout(() => {
+    isOpen.value = true
+  }, 150)
+}
+
+const handleClickOutside = (e: MouseEvent) => {
+  if (menuRef.value && !menuRef.value.contains(e.target as Node)) {
+    isOpen.value = false
+  }
+}
+
+onMounted(() => {
+  document.addEventListener('click', handleClickOutside)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', handleClickOutside)
+})
+</script>
+
+<style scoped>
+.container-wrapper {
+  position: relative;
+  display: flex;
+  gap: 4px;
+}
+
+.filter-menu-heading {
+  padding: 12px 24px 12px 12px;
+  display: flex;
+  gap: 4px;
+}
+
+.base-menu-container .header {
+  display: flex;
+  border-bottom: 1px solid var(--color-border);
+  gap: 18px;
+  padding: 12px 10px;
+}
+
+.base-menu-container .scroll-area {
+  overflow-y: scroll;
+  flex: 1;
+}
+
+.check-box {
+  width: 16px;
+  height: 16px;
+  margin-right: 10px;
+}
+
+.options {
+  overflow-y: scroll;
+}
+
+.options > * {
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  gap: 24px;
+}
+
+.options > *:hover {
+  background-color: var(--color-onhover);
+}
+
+.menu-container-buttons {
+  color: var(--color-blue);
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 10px;
+  padding: 4px 12px;
+  border-top: 1px solid var(--color-border);
+}
+
+.menu-container-buttons div {
+  padding: 6px 12px;
+  font-weight: 500;
+}
+
+.menu-container-buttons div:hover {
+  background-color: var(--color-blue-light);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: scale(0.5);
+  opacity: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  transform: scale(1);
+  opacity: 1;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+</style>
--- src/components/main/contact/ContactTable.vue
+++ src/components/main/contact/ContactTable.vue
@@ -1,16 +1,58 @@
 <template>
   <input type="checkbox" class="check-box" />
 
-  <div class="table-heading" v-for="header in tableHeaders" :key="header">
-    <p>{{ header }}</p>
-    <Icon icon="tabler:filter-2" width="16" height="16" />
+  <div class="table-heading" v-for="(header, index) in tableHeaders" :key="header" ref="headerEl">
+    <base-filter-menu
+      :header="header"
+      :position="index === tableHeaders.length - 1 ? 'bottom-end' : 'bottom-start'"
+      :target="headerEl"
+    >
+      <template #title>
+        <p class="header">{{ header }}</p>
+      </template>
+      <template #trigger>
+        <Icon icon="tabler:filter-2" width="16" height="16" />
+      </template>
+
+      <template #filter-menu>
+        <div v-for="(name, i) in getUsersProps[index]?.value" :key="i">
+          <input type="checkbox" class="check-box" />
+          <div>
+            {{ name }}
+          </div>
+        </div>
+      </template>
+    </base-filter-menu>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
 import { Icon } from '@iconify/vue'
+import { useUserStore } from '@/store/user'
+import BaseFilterMenu from '@/components/UI/BaseFilterMenu.vue'
 
-const tableHeaders = ['Name', 'Company', 'Status', 'Assigned To', 'Phone', 'Email']
+const headerEl = ref<HTMLElement[] | null>()
+
+const tableHeaders = ['Name', 'Company', 'Status', 'Assigned to', 'Phone', 'Email']
+const {
+  getUsersName,
+  getUsersAssignedTo,
+  getUsersCompany,
+  getUsersEmail,
+  getUsersPhone,
+  getUsersStatus,
+} = storeToRefs(useUserStore())
+
+const getUsersProps = [
+  getUsersName,
+  getUsersCompany,
+  getUsersStatus,
+  getUsersAssignedTo,
+  getUsersPhone,
+  getUsersEmail,
+]
 </script>
 
 <style scoped>
@@ -20,8 +62,7 @@ const tableHeaders = ['Name', 'Company', 'Status', 'Assigned To', 'Phone', 'Emai
   cursor: pointer;
   display: flex;
   align-items: center;
-  gap: 2px;
-  padding: 12px 24px 12px 12px;
+  gap: 4px;
 }
 
 .table-heading:hover {
