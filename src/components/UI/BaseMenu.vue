7d5bfb40ae117159688feaf1a075fed7b5493e86
Rithik <rithikpvijay2002@gmail.com>
2025-11-27 14:32:16 +0530

    feat(playback-speed): implement playback speed for the video player

--- src/components/UI/BaseMenu.vue
+++ src/components/UI/BaseMenu.vue
@@ -4,7 +4,19 @@
       <slot name="title"></slot>
     </div>
     <Transition name="fade">
-      <div class="menu" :style="menuStyle" v-if="isOpen" @click="handleMenuClick">
+      <div
+        class="menu"
+        :style="{
+          ...menuStyle,
+          '--menu-hover': props.hoverColor || 'var(--color-heading-hover)',
+          backgroundColor: props.backgroundColor,
+          color: props.color,
+          top: props.top,
+          left: props.left,
+        }"
+        v-if="isOpen"
+        @click="handleMenuClick"
+      >
         <slot name="menu"></slot>
       </div>
     </Transition>
@@ -16,25 +28,40 @@ import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
 import type { CSSProperties } from 'vue'
 
 interface Props {
-  position?: 'bottom-left' | 'bottom-right' | 'top-left' | 'top-right'
+  backgroundColor?: string
+  color?: string
+  hoverColor?: string
+  top?: string
+  left?: string
+  width: string
+  position?:
+    | 'bottom-left'
+    | 'bottom-right'
+    | 'top-left'
+    | 'top-right'
+    | 'top-center'
+    | 'bottom-center'
 }
 
 const props = defineProps<Props>()
-
 const isOpen = ref(false)
+const emit = defineEmits<{ (e: 'menuClose'): void; (e: 'menuToggle', value: boolean): void }>()
 const menuRef = ref<HTMLElement | null>(null)
 
 function handleMenuClick() {
   isOpen.value = false
+  emit('menuClose')
 }
 
 function handleMenuToggle() {
   isOpen.value = !isOpen.value
+  emit('menuToggle', isOpen.value)
 }
 
 function handleClickOutside(e: MouseEvent) {
   if (menuRef.value && !menuRef.value.contains(e.target as Node)) {
     isOpen.value = false
+    emit('menuClose')
   }
 }
 
@@ -54,7 +81,6 @@ const menuStyle = computed(() => {
     fontSize: '13px',
     boxShadow: 'var(--shadow-menu)',
     zIndex: 10,
-    backgroundColor: 'var(--color-primary)',
   })
 
   if (props.position) {
@@ -66,6 +92,13 @@ const menuStyle = computed(() => {
     }
     if (props.position === 'top-left') {
       return { ...baseStyle.value, bottom: '100%', left: '0' }
+    }
+
+    if (props.position === 'top-center') {
+      return { ...baseStyle.value, bottom: '100%', left: '50%', transform: 'translateX(-50%)' }
+    }
+    if (props.position === 'bottom-center') {
+      return { ...baseStyle.value, top: '100%', left: '50%', transform: 'translateX(-50%)' }
     }
   }
   return { ...baseStyle.value, top: '100%', left: '0' }
@@ -88,7 +121,11 @@ onBeforeUnmount(() => {
 }
 
 .active {
-  background-color: var(--color-heading-hover);
+  background-color: var(--color-menu-hover);
+}
+
+.menu {
+  background-color: var(--color-primary);
 }
 
 .menu > * {
@@ -98,7 +135,7 @@ onBeforeUnmount(() => {
 }
 
 .menu > *:hover {
-  background-color: var(--color-heading-hover);
+  background-color: var(--menu-hover);
 }
 
 .menu > *:active {
